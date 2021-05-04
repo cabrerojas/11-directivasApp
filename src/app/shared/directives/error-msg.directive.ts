effd5faf1d6214fcf7882add5ad178bc091064a1
cabrerojas <gcabrerojas@gmail.com>
2021-05-03 22:30:49 -0400

    parámetros directivas
    Se envían parámetros a través de la directiva con un Input para modificar el DOM del HTML

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,17 +1,38 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[error-msg]'
+  selector: '[appErrorMsg]'
 })
 export class ErrorMsgDirective implements OnInit{
 
-  constructor() {
-    console.log('Directiva');
+  htmlElement: ElementRef<HTMLElement>;
+  @Input() color = 'red';
+  @Input() mensaje = 'Este campo es requerido';
+
+  constructor( private el: ElementRef<HTMLElement> ) {
+
+    this.htmlElement = el;
+
    }
 
   ngOnInit(): void {
-    console.log('Directiva');
 
+    this.setColor();
+    this.setMensaje();
+    this.setClase();
+
+  }
+
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this.color;
+  }
+
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this.mensaje;
+  }
+
+  setClase(): void {
+    this.htmlElement.nativeElement.classList.add('form-text');
   }
 
 }
