7fa3c576dfb464734efc835bf1c620c29f59f5e0
cabrerojas <gcabrerojas@gmail.com>
2021-05-03 21:50:18 -0400

    estructura del proyecto
    se generan los modulos con sus respectivos componentes y se maquetea la pagina de agregar productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'producto',
+    loadChildren: () => import('./productos/productos.module').then(m => m.ProductosModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'producto'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
